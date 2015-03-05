085528b1fb8957a749e709048d79a1a84f7251eb
Allen Zeng <suyuz@andrew.cmu.edu>
2015-03-05 17:10:55 -0500

    Add thumbnail and homepage.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,24 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+  def home
+    
+  end
   # GET /posts
-  # GET /posts.json
+  # GET /posts.json 
   def index
     @posts = Post.all.paginate(:page => params[:page], :per_page => 30)
+    @posts = @posts.where(bedrooms: params["bedrooms"]) if params["bedrooms"].present?
+    @posts = @posts.where(bathrooms: params["bathrooms"]) if params["bathrooms"].present?
+    @posts = @posts.where(neighborhood: params["neighborhood"]) if params["neighborhood"].present?
+    @posts = @posts.where("price > ?", params["min_price"]) if params["min_price"].present?
+    @posts = @posts.where("price < ?", params["max_price"]) if params["max_price"].present?
+    @posts = @posts.where("sqft > ?", params["min_sqft"]) if params["min_sqft"].present?
+    @posts = @posts.where("sqft < ?", params["max_sqft"]) if params["max_sqft"].present?
+    @posts = @posts.where(cats: params["cats"]) if params["cats"].present?
+    @posts = @posts.where(dogs: params["dogs"]) if params["dogs"].present?
+    @posts = @posts.where(w_d_in_unit: params["w_d_in_unit"]) if params["w_d_in_unit"].present?
+    @posts = @posts.where(street_parking: params["street_parking"]) if params["street_parking"].present?
   end
 
   # GET /posts/1
